da7859fc4cb7481779f4a5de6666cdd4482b209f
Wojciech Piekos <wojciech.piekos@gmail.com>
2024-01-07 13:07:40 +0100

    exercise next

---
+++ next06/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 🖐️&nbsp;
+        <span className="whitespace-nowrap">
+          I'm <span className="font-bold">Wojtek</span>
+        </span>
+      </p>
+    </main>
+  )
+}
